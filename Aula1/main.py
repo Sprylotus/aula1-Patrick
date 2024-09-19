caa0cf9e44047eff65b9239766a402009ab6bc55
Sprylotus <51134552024.01@senacrjedu.local>
2024-09-19 19:10:42 -0300

    diretório da aula 01

---
+++ Aula1/main.py
@@ -0,0 +1,41 @@
+# print("Hello World!!!")
+# print('Patrick')
+# string - cadeia de caracteres
+# int - Números inteiros
+# float - Números reais
+# bool - True | False
+# CTRL + ALT + N - rodar
+#  CTRL + S - salvar
+# +
+# -
+# *
+# /
+# %- módulo ( resto da divisão)
+# // - parte inteira da divisão
+# ano_nascimento = 1981
+# ano_atual = 2024
+# idade = ano_atual - ano_nascimento
+# print(idade)
+
+# n1 = 5 
+# n2 = 2
+# resultado = n1 // n2
+# print(resultado)
+# assunto = 'Algoritmos em Python'
+# print(assunto)
+# nome1 = 'Marcos!'
+# print('Ola', nome1)
+# nome3 = 'Paulo'
+# cor2 = 'Azul'
+# print(nome3, cor2)
+# cor = 'preto,'
+# nome2 = 'Patrick,'
+# profissão = 'Analista de Dados'
+# casa = 'Porto Velho'
+# print('Ola',nome2,'a sua profissao eh',profissão,'e voce mora em',casa)
+usuario = input('Informe seu nome: ')
+ano_nascimento = int(input('Informe seu ano de nascimento: '))
+altura = float(input('Informe sua altura: '))
+idade = 2024 - ano_nascimento
+print(f'Seu nome eh {usuario}, seu ano de nascimneto eh {ano_nascimento} e sua altura eh {altura}')
+print(f'Sua idade eh {idade}')
